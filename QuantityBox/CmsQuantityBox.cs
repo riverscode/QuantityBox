d58fb1676dcf51ee6a5d659aa24e6a11d223f5ce
Rivers Code <riverscode@gmail.com>
2021-12-06 13:55:21 -0500

    Agregar archivos de proyecto.

---
+++ QuantityBox/CmsQuantityBox.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+// Revit API Namespaces
+using Autodesk.Revit.Attributes;
+using Autodesk.Revit.DB;
+using Autodesk.Revit.UI;
+using Autodesk.Revit.UI.Selection;
+
+namespace QuantityBox
+{
+    public class CmsQuantityBox : IExternalCommand
+    {
+        public Result Execute(ExternalCommandData commandData, ref string message, ElementSet elements)
+        {
+            TaskDialog.Show("Title", "Message");
+            return Result.Succeeded;
+        }
+    }
+}
